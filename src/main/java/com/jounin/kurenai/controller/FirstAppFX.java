b88a915dba9ad607ad4d0f56aab064429dca6cc9
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2023-01-06 00:52:15 -0300

    feature: Lesson 373 - Implementing the first JavaFX screen.

--- src/main/java/com/jounin/kurenai/controller/FirstAppFX.java
+++ src/main/java/com/jounin/kurenai/controller/FirstAppFX.java
@@ -1,2 +1,41 @@
-package com.jounin.kurenai.controller;public class FirstAppFX {
+package com.jounin.kurenai.controller;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class FirstAppFX extends Application {
+
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+        System.out.println("FirstAppFX - start(): BEGIN");
+
+        final Button buttonLetterA = new Button("A");
+        final Button buttonLetterB = new Button("B");
+        final Button buttonLetterC = new Button("C");
+
+        buttonLetterA.setOnAction(event -> System.out.println("Letter A was pressed."));
+        buttonLetterB.setOnAction(event -> System.out.println("Letter B was pressed."));
+        buttonLetterC.setOnAction(event -> System.exit(0));
+
+        final HBox horizontalBox = new HBox();
+        horizontalBox.setAlignment(Pos.CENTER);
+        horizontalBox.getChildren().add(buttonLetterA);
+        horizontalBox.getChildren().add(buttonLetterB);
+        horizontalBox.getChildren().add(buttonLetterC);
+
+        final int widthScene = 100;
+        final int heightScene = 400;
+        final Scene uniqueScene = new Scene(horizontalBox,widthScene,heightScene);
+
+        primaryStage.setScene(uniqueScene);
+        primaryStage.show();
+        System.out.println("FirstAppFX - start(): END");
+    }
+
+    public static void main(String[] args) { launch(); }
 }
