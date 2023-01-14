c768aca94f4327307108f79004c0b88a1ec5a5c8
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2023-01-14 17:45:10 -0300

    refactor: Lesson 376 - Adding Style to Counter Elements.

--- src/main/java/com/jounin/kurenai/controller/Counter.java
+++ src/main/java/com/jounin/kurenai/controller/Counter.java
@@ -20,6 +20,9 @@ public class Counter extends Application {
         final Label labelTitle = new Label("Counter");
         final Label labelNumber = new Label("0");
 
+        labelTitle.getStyleClass().add("style-title");
+        labelNumber.getStyleClass().add("style-number");
+
         final Button buttonIncrement = new Button("+");
         final Button buttonDecrement = new Button("-");
 
@@ -44,15 +47,21 @@ public class Counter extends Application {
         horizontalBox.getChildren().add(buttonIncrement);
 
         final VBox mainVerticalBox = new VBox();
+        mainVerticalBox.getStyleClass().add("contents");
         mainVerticalBox.setAlignment(Pos.CENTER);
         mainVerticalBox.setSpacing(10);
         mainVerticalBox.getChildren().add(labelTitle);
         mainVerticalBox.getChildren().add(labelNumber);
         mainVerticalBox.getChildren().add(horizontalBox);
 
+        final String pathStyleFile = getClass().getResource("/Counter.css").toExternalForm();
+        final String siteGoogleFonts = "https://fonts.googleapis.com/css?family=Oswald";
+
         final int widthScene = 400;
         final int heightScene = 400;
         final Scene mainScene = new Scene(mainVerticalBox, widthScene,heightScene);
+        mainScene.getStylesheets().add(pathStyleFile);
+        mainScene.getStylesheets().add(siteGoogleFonts);
 
         primaryStage.setScene(mainScene);
         primaryStage.show();
