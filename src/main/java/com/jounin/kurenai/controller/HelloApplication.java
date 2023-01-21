35ae6bee2fa4d327904084532b3d6b58c9df4feb
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2023-01-21 20:12:27 -0300

    feature: Lesson 391 - Adding styles a css file and configuration with an fxml file.

--- src/main/java/com/jounin/kurenai/controller/HelloApplication.java
+++ src/main/java/com/jounin/kurenai/controller/HelloApplication.java
@@ -10,7 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/jounin/kurenai/hello-view.fxml"));
+
+        // Removida a referencia de classe main no arquivo de configuracao do Maven - pom.xml
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
--- src/main/java/com/jounin/kurenai/controller/fxml/AppFXML.java
+++ src/main/java/com/jounin/kurenai/controller/fxml/AppFXML.java
@@ -14,12 +14,15 @@ public class AppFXML extends Application {
     public void start(final Stage primaryStage) throws Exception {
         System.out.println("AppFXML - start(): BEGIN");
         try {
-            final URL fileFXML = getClass().getResource("login.fxml");
-            System.out.println("AppFXML - start(): " + fileFXML.toString());
-            final GridPane root = FXMLLoader.load(fileFXML);
+            final String fileStyleCss = getClass().getResource("/Login.css").toString();
+            System.out.println("AppFXML - start(): fileStyleCss: " + fileStyleCss);
+            final URL fileConfigurationFXML = getClass().getResource("/Login.fxml");
+            System.out.println("AppFXML - start(): fileConfigurationFXML: " + fileConfigurationFXML.toString());
+            final GridPane root = FXMLLoader.load(fileConfigurationFXML);
             final int widthScene = 350;
             final int heightScene = 400;
             final Scene mainScene = new Scene(root,widthScene,heightScene);
+            mainScene.getStylesheets().add(fileStyleCss);
             primaryStage.setResizable(false);
             primaryStage.setTitle("Login Screen");
             primaryStage.setScene(mainScene);
