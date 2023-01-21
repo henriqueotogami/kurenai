01e37810fe6875f023c47a6fcae1ff06ee583096
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2023-01-21 16:33:40 -0300

    feature: Lesson 389 - Implementation of the AppXMLS login screen strutcture.

--- src/main/java/com/jounin/kurenai/controller/fxml/AppFXML.java
+++ src/main/java/com/jounin/kurenai/controller/fxml/AppFXML.java
@@ -1,13 +1,33 @@
 package com.jounin.kurenai.controller.fxml;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class AppFXML extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(final Stage primaryStage) throws Exception {
         System.out.println("AppFXML - start(): BEGIN");
-        primaryStage.show();
+        try {
+            final URL fileFXML = getClass().getResource("login.fxml");
+            System.out.println("AppFXML - start(): " + fileFXML.toString());
+            final GridPane root = FXMLLoader.load(fileFXML);
+            final int widthScene = 350;
+            final int heightScene = 400;
+            final Scene mainScene = new Scene(root,widthScene,heightScene);
+            primaryStage.setResizable(false);
+            primaryStage.setTitle("Login Screen");
+            primaryStage.setScene(mainScene);
+            primaryStage.show();
+        } catch (final IOException ioException) {
+            ioException.getStackTrace();
+            throw new RuntimeException(ioException);
+        }
         System.out.println("AppFXML - start(): END");
     }
 
